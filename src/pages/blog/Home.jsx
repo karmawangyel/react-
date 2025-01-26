c93005dc1826b8215bc327cf9310fba7dc8a8a08
karma <karmawangyelsherpa@gmail.com>
2025-01-26 16:24:23 +0545

    file updated

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
 import Card from "./components/Card";
+import { baseUrl } from "../../config";
+import axios from "axios";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+  const fetchBlogs = async () => {
+    const response = await axios.get(`${baseUrl}/blog`);
+    if (response.status === 200) {
+      setBlogs(response.data.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
   return (
     <>
       <Layout>
-        <div className="home my-5 flex space-x-4 flex-wrap justify-center align-middle gap-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="home my-5 flex gap-10 flex-wrap justify-center align-middle gap-3">
+          {blogs.length > 0 &&
+            blogs.map((blog) => {
+              console.log(blog);
+              return <Card blog={blog} />;
+            })}
         </div>
       </Layout>
     </>
